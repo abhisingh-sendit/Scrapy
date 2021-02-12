61972efaa9e738cf06e5d631c1f9c47fed21460b
Abhinav Singh <abhisingh2412@gmail.com>
2021-02-12 11:25:58 -0500

    Add files via upload

---
+++ imdb/items.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+
+# Define here the models for your scraped items
+#
+# See documentation in:
+# https://doc.scrapy.org/en/latest/topics/items.html
+
+import scrapy
+
+class ImdbItem(scrapy.Item):
+    title = scrapy.Field()
+    year = scrapy.Field()
+    rank = scrapy.Field()
+    rating = scrapy.Field()
+
+class MetaCriticItem(scrapy.Item):
+    title = scrapy.Field()
+    year = scrapy.Field()
+    rank = scrapy.Field()
+    rating = scrapy.Field()
+
+
+
+
